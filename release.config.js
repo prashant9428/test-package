4e986f68e00c604aaece5b51639b9d0f4592f3a3
prashant9428 <pathakprashant841@gmail.com>
2022-01-24 23:15:45 +0530

    fix added workflow

--- release.config.js
+++
@@ -1,9 +0,0 @@
-module.exports  = {
-    branches: "master",
-    repositoryUrl:"https://github.com/prashant9428/test-package",
-    plugins:[
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/github'
-    ]
-}
