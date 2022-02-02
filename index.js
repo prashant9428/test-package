c70d18613e6cdd158ede93276b6eaa31621ded96
prashant9428 <pathakprashant841@gmail.com>
2022-02-02 18:09:58 +0530

    fix: index file changes

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express")
-require('dotenv').config()
 const app = express()
 
 app.get("/",(req,res)=>{
-    res.send("working")
+    res.send(`working ${process.env.github_tag}`)
 })
 
 app.listen(3000,()=>{
