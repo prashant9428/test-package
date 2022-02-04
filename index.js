3e8d14118a1c8147334286546d863f2f179e102c
prashant9428 <pathakprashant841@gmail.com>
2022-02-04 08:28:46 +0530

    fix: created a release api

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ app.get("/",async (req,res)=>{
 
 })
 
-app,get("/tag",async (req,res)=>{
+app,get("/release",async (req,res)=>{
+    console.log("user added")
     res.status(400).json({
         status:"working"
     });
